00d176523782d619f4107c15587ea1f3fcd1d77d
Victor <vitchor@gmail.com>
2012-08-19 21:42:20 -0300

    More absolute path fixing

--- mysite/settings.py
+++ mysite/settings.py
@@ -64,7 +64,7 @@ STATIC_ROOT = '/static/'
 STATIC_URL = '/static/'
 
 # Additional locations of static files
-STATICFILES_DIRS = (os.path.join('/Users/marciarozenfeld/DynaFocusServer/mysite/', 'uploader', 'static'),)
+STATICFILES_DIRS = (os.path.join('/home/ubuntu/mysite/', 'uploader', 'static'),)
 
 # List of finder classes that know how to find static files in
 # various locations.
