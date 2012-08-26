ef0bb2f6dc2a882d93e953b11fc92cdd50d64987
Alexandre Cordeiro <alexandre@pompeii.local>
2012-08-26 19:45:01 -0300

    fof_navigator now references static file fof.js and (probably) is now cuter than ever

--- static/fof.js
+++ static/fof.js
@@ -1,6 +1,3 @@
-
-
-
 var newImage;
 var oldImage;
 
@@ -16,44 +13,44 @@ var homediv;
 function homefade() {
     if (!homediv) {
         homediv = document.getElementById("homeimg");
-        
+
     } else {
-        
+
         if (newImage) {
-            
+
             if (oldImage) {
                 homediv.removeChild(oldImage);
             }
-            
+
             oldImage = newImage;
         }
-        
+
         newImage = document.createElement('img');
         newImage.src = imgsArray[newImageIndex];
         newImage.id = "newImg"
         newImage.style.position = 'absolute';
         newImage.style.opacity = "0";
         newImage.style.filter = "alpha(opacity=0)";
-        newImage.width = "850"
-        
+        newImage.width = "600"
+
         if (oldImage) {
             var big_coordinates=getXYpos(oldImage);
             var bp_x = big_coordinates['x'];
             var bp_y = big_coordinates['y'];
-            
+
             newImage.style.left = bp_x + 'px';
             newImage.style.top = bp_y + 'px';
         }
-        
+
         homediv.appendChild(newImage);
-        
+
         if (newImageIndex == (imgsArray.length-1)) {
             newImageIndex = 0;
-            
+
         } else {
             newImageIndex++;
         };
-        
+
         timer = setInterval("homefadeTrans();", 0.25);    
     }
 }
@@ -61,19 +58,19 @@ function homefade() {
 function homefadeTrans() {
     if (newImageOpacity >= 100) {
         clearTimeout(timer);
-        
+
         timer = 0;
         newImageOpacity = 0;
         oldImageOpacity = 100;
-        
+
         setTimeout("homefade();", 700);
-        
+
     } else {
-    
+
         newImage.style.opacity = newImageOpacity/100;
         newImage.style.filter = "alpha(opacity="+newImageOpacity+")";
         newImageOpacity = newImageOpacity+2.5;
-    
+
         if (oldImage) {
             //oldImage.style.opacity = oldImageOpacity/100;
             //oldImage.style.filter = "alpha(opacity="+oldImageOpacity+")";
@@ -83,17 +80,18 @@ function homefadeTrans() {
 }
 
 function getXYpos(elem) {
-    
+
    if (!elem) {
       return {"x":0,"y":0};
    }
-   
+
    var xy = {"x":elem.offsetLeft,"y":elem.offsetTop}
    var par = getXYpos(elem.offsetParent);
-   
+
    for (var key in par) {
       xy[key] += par[key];
    }
-   
+
    return xy;
 }
+
