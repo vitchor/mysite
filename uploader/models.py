6e2e5b06c256aef0185a3f7740afd7e3ff298b3e
Victor <vitchor@gmail.com>
2012-10-07 19:38:29 -0300

    Ordering featured fofs by rank

--- uploader/models.py
+++ uploader/models.py
@@ -24,6 +24,7 @@ class FOF(models.Model):
 
 class Featured_FOF(models.Model):
     fof = models.ForeignKey(FOF)
+    rank = models.IntegerField()
     def __unicode__(self):
         return self.fof.name
 
