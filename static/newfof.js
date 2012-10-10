47387010e7e917aa21d32a2ce26bbc3e560671bc
Victor <vitchor@gmail.com>
2012-10-10 01:10:49 -0300

    Created tempImages array

--- static/newfof.js
+++ static/newfof.js
@@ -14,6 +14,13 @@ function homefade() {
     if (!threadRunning) {
         threadRunning = true;
         
+        var tempImg = [];
+
+        for(var x=0;x<imgsArray.length;x++) {
+            tempImg[x] = new Image()
+            tempImg[x].src = imgsArray[x]
+        }
+        
         if (imgsArray.length > 1) {
         
             if (!(frontImage && backImage)) {
@@ -21,12 +28,15 @@ function homefade() {
                 backImage = document.getElementById('backImage');
             }
         
-            backImage.src = imgsArray[backImageIndex];
+            backImage.src = tempImg.src;
         
             frontImage.style.opacity = "0";
+            frontImage.style.filter = "alpha(opacity="+0.0+")";            
+            
             backImage.style.opacity = "1";
-        
-            frontImage.src = imgsArray[frontImageIndex];
+            backImage.style.filter = "alpha(opacity="+1.0+")"; 
+                   
+            
         
             if (frontImageIndex == (imgsArray.length - 1)) {
                 frontImageIndex = 0;
@@ -42,8 +52,9 @@ function homefade() {
                 }
             
             }
-        
+            
             timer = setInterval("homefadeTrans();", 10);
+            frontImage.src = tempImg.src;            
         } else {
             frontImage = document.getElementById('frontImage');
             frontImage.src = imgsArray[0];
@@ -63,7 +74,7 @@ function homefadeTrans() {
     } else {
 
         frontImage.style.opacity = newImageOpacity/100.0;
-        frontImage.style.filter = "alpha(opacity="+newImageOpacity+")";
+        frontImage.style.filter = "alpha(opacity="+newImageOpacity/100.0+")";
         newImageOpacity = newImageOpacity + 1;
         
     }
