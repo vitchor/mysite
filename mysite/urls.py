e09cf1997e17ee21aa4a285cf5c50e7c945444a3
Alexandre Cordeiro <alexandre@pompeii.local>
2012-11-08 02:31:59 -0200

    Redirecting home to Facebook page

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import patterns, include, url
+from django.views.generic.simple import direct_to_template
 
 from django.contrib import admin
 admin.autodiscover()
@@ -7,6 +8,7 @@ urlpatterns = patterns('',
     url(r'^polls/', include('polls.urls')),
     url(r'^uploader/', include('uploader.urls')),    
     url(r'^admin/', include(admin.site.urls)),
+    url(r"^$", direct_to_template, {"template": "index.html"}),
 )
 
 #urlpatterns += patterns('', (
